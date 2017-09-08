54df9580ea93461a360f7e0a751f4d2ac3390802
aghumnar <ashish_ghumnar_tp@bmc.com>
2017-09-08 09:30:30 +0530

    fixed some defects in app

--- app/tic-tac-toe/services/validate-matrix.service.js
+++ app/tic-tac-toe/services/validate-matrix.service.js
@@ -4,49 +4,65 @@
     angular.module('ticTacToe')
         .factory('validateMatrix', [function () {
             function validate(tickMatrix) {
-                if (tickMatrix.length < 3 ) {
+                if (tickMatrix.length < 3) {
                     return false;
                 }
 
                 return visitColumn(tickMatrix) || visitRows(tickMatrix) || visitDiagonals(tickMatrix);
             }
 
-            function visitColumn(tickMatrix) {
-                var firstCellPos = tickMatrix[0].col,
-                    isValid = true;
+            function visitColumn(tickMatrix, matrix) {
+                var isValid = true;
 
-                angular.forEach(tickMatrix, function (item, index) {
-                    if (item.col !== firstCellPos) {
+                for (var item = 0 ; item <3 ; item++) {
+                    var count = getPropertyCount(tickMatrix, 'col', item);
+                    if (count === 3) {
+                        isValid = true;
+                        break;
+                    } else {
                         isValid = false;
                     }
-                });
+                }
 
                 return isValid;
             }
 
             function visitRows(tickMatrix) {
-                var firstCellPos = tickMatrix[0].row,
-                    isValid = true;
+                var isValid = true;
 
-                angular.forEach(tickMatrix, function (item, index) {
-                    if (item.row !== firstCellPos) {
+                for (var item = 0 ; item <3 ; item++) {
+                    var count = getPropertyCount(tickMatrix, 'row', item);
+                    if (count === 3) {
+                        isValid = true;
+                        break;
+                    } else {
                         isValid = false;
                     }
-                });
+                }
 
                 return isValid;
             }
 
             function visitDiagonals(tickMatrix) {
-                var isValid = true;
+                var count = 0;
 
                 angular.forEach(tickMatrix, function (item) {
-                    if (item.row !== item.col) {
-                        isValid = false;
+                    if (item.row === item.col) {
+                        count++;
                     }
                 });
 
-                return isValid;
+                return count === 3;
+            }
+
+
+
+            function getPropertyCount(array, prop, value) {
+                var filterArray = array.filter(function (item) {
+                    return item[prop] === value;
+                });
+
+                return filterArray.length;
             }
 
             return {
